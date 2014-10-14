999cf43caa4fa1ac6466834a1c40f86599e56d86
gregasman <greg@asman.com>
2014-10-14 09:34:59 -0400

    working on nav

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root to: "home#index"
+  #root to: "home#index" 
+  get 'pages/home' => 'high_voltage/pages#show', id: 'home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
