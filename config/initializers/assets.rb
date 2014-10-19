19739d932c037671712cfd9cac8c77a95394aacc
gregasman <greg@asman.com>
2014-10-18 21:27:10 -0400

    more bootstrap learning

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,5 +4,10 @@
 Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
+##bootstrap.min.js
+##html5shiv.js
+##jquery.min.js
+##scripts.js
+
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
